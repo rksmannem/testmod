d6654da522da3c1d54ccdba78fece1289d811d89
Ramakrishna Mannem <ramakrishna.mannem@rewardstyle.com>
2020-03-05 12:04:30 -0600

    change to support multi language

--- testmod.go
+++ testmod.go
@@ -1,7 +1,22 @@
 package testmod
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func Hi(name string) string {
-	return fmt.Sprintf("Hi, %s!\n", name)
+// Hi ....
+func Hi(name, lang string) (string, error) {
+	switch lang {
+	case "en":
+		return fmt.Sprintf("Hi, %s!\n", name), nil
+	case "pt":
+		return fmt.Sprintf("Oi, %s!\n", name), nil
+	case "es":
+		return fmt.Sprintf("¡Hola, %s!\n", name), nil
+	case "fr":
+		return fmt.Sprintf("Bonjour, %s!\n", name), nil
+	default:
+		return "", errors.New("unknown language")
+	}
 }
